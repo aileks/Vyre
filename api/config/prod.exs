e280f78c3a8a6ec4da3dce363368c4894131c90c
Aaliyah Harper <aeverly14@pm.me>
2025-03-07 12:35:11 -0500

    ssl debugging

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -10,10 +10,7 @@ config :logger, level: :info
 config :api, Api.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL") || raise("DATABASE_URL is missing"),
-  ssl: [
-    verify: :verify_peer,
-    cacertfile: "/etc/ssl/certs/prod-ca-2021.crt"
-  ],
+  ssl: true,
   parameters: [
     search_path: System.get_env("SCHEMA") || "vyre"
   ]
