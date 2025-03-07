00b59299d9ba15017dbfc3fae909fe0390cb727a
Aaliyah Harper <aeverly14@pm.me>
2025-03-07 17:16:25 -0500

    clean up prod.exs and runtime.exs

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -1,19 +1,6 @@
 import Config
 
-config :api, Api.Accounts.Guardian, secret_key: System.get_env("GUARDIAN_SECRET_KEY")
-
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :warn
 
-# Runtime production configuration, including reading
-# of environment variables, is done on config/runtime.exs.
-config :api, Api.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL") || raise("DATABASE_URL is missing"),
-  ssl: [
-    verify: :verify_peer,
-    cacertfile: "/etc/ssl/certs/prod-ca-2021.crt",
-    server_name_indication: String.to_charlist(URI.parse(System.get_env("DATABASE_URL")).host)
-  ],
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  timeout: 30000
+config :bcrypt_elixir, :log_rounds, 12
--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -1,5 +1,7 @@
 import Config
 
+config :logger, level: :info
+
 # config/runtime.exs is executed for all environments, including
 # during releases. It is executed after compilation and before the
 # system starts, so it is typically used to load production configuration
@@ -39,38 +41,21 @@ if config_env() == :prod do
       cacertfile: "/etc/ssl/certs/prod-ca-2021.crt",
       server_name_indication: String.to_charlist(URI.parse(System.get_env("DATABASE_URL")).host)
     ],
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    pool_size: 10,
     timeout: 30000
-
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
-  # secret_key_base =
-  #   System.get_env("SECRET_KEY_BASE") ||
-  #     raise """
-  #     environment variable SECRET_KEY_BASE is missing.
-  #     You can generate one by calling: mix phx.gen.secret
-  #     """
 
   guardian_secret_key =
     System.get_env("GUARDIAN_SECRET_KEY") ||
-      if config_env() in [:dev, :test] do
-        # Fallback for dev/test if not in .env
-        "insecure_key_for_dev_environment_only"
-      else
-        raise """
-        environment variable GUARDIAN_SECRET_KEY is missing.
-        You can generate one by calling: mix phx.gen.secret 32
-        """
-      end
+      raise """
+      environment variable GUARDIAN_SECRET_KEY is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
 
   config :api, Api.Accounts.Guardian,
     issuer: "api",
     secret_key: guardian_secret_key
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "vyre.app"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :api, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -78,68 +63,8 @@ if config_env() == :prod do
   config :api, ApiWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
     secret_key_base: guardian_secret_key
-
-  if config_env() in [:dev, :test] do
-    config :bcrypt_elixir, :log_rounds, 4
-  else
-    config :bcrypt_elixir, :log_rounds, 12
-  end
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :api, ApiWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your config/prod.exs,
-  # ensuring no data is ever sent via http, always redirecting to https:
-  #
-  #     config :api, ApiWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :api, Api.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 end
