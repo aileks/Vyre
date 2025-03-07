ed0b1211580b613c27fe7d6b180baf0933fa89f0
Aaliyah Harper <aeverly14@pm.me>
2025-03-07 13:43:09 -0500

    basic ssl for debugging

--- api/config/runtime.exs
+++ api/config/runtime.exs
@@ -32,10 +32,7 @@ if config_env() == :prod do
 
   config :api, Api.Repo,
     adapter: Ecto.Adapters.Postgres,
-    ssl: [
-      verify: :verify_peer,
-      cacertfile: "/etc/ssl/certs/ca-certificates.crt"
-    ],
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
