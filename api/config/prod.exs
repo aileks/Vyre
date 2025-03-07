196756de9f51fee790505f5df576aac54f1f2e9f
Aaliyah Harper <aeverly14@pm.me>
2025-03-07 17:41:16 -0500

    prod.exs update

--- api/config/prod.exs
+++ api/config/prod.exs
@@ -1,6 +1,17 @@
 import Config
 
-# Do not print debug messages in production
-config :logger, level: :warn
+config :api, Api.Accounts.Guardian, secret_key: System.get_env("GUARDIAN_SECRET_KEY")
 
-config :bcrypt_elixir, :log_rounds, 12
+# Do not print debug messages in production
+config :logger, level: :info
+
+config :api, Api.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL") || raise("DATABASE_URL is missing"),
+  ssl: [
+    verify: :verify_peer,
+    cacertfile: "/etc/ssl/certs/prod-ca-2021.crt",
+    server_name_indication: String.to_charlist(URI.parse(System.get_env("DATABASE_URL")).host)
+  ],
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  timeout: 30000
