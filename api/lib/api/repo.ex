f041d8c28693fa29cd0db193ba66e0e338d111b1
Aaliyah Harper <aeverly14@pm.me>
2025-03-07 18:40:49 -0500

    add to repo

--- api/lib/api/repo.ex
+++ api/lib/api/repo.ex
@@ -2,4 +2,15 @@ defmodule Api.Repo do
   use Ecto.Repo,
     otp_app: :api,
     adapter: if(Mix.env() == :prod, do: Ecto.Adapters.Postgres, else: Ecto.Adapters.SQLite3)
+
+  def init(_type, config) do
+    config =
+      Keyword.put(
+        config,
+        :url,
+        System.get_env("DATABASE_URL") || Keyword.get(config, :url)
+      )
+
+    {:ok, config}
+  end
 end
