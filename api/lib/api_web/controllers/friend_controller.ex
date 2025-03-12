d81d58a855fceb3477227d44d3948a5c0295d608
Aaliyah Harper <aeverly14@pm.me>
2025-03-12 13:33:27 -0400

    add rest of friend logic

--- api/lib/api_web/controllers/friend_controller.ex
+++ api/lib/api_web/controllers/friend_controller.ex
@@ -7,29 +7,29 @@ defmodule ApiWeb.FriendController do
   action_fallback(ApiWeb.FallbackController)
 
   def index(conn, _params) do
-    friends = Friends.list_friends()
-    render(conn, :index, friends: friends)
+    friends = Friends.get_friends(conn.assigns.current_user.id)
+    render(conn, "friend.json", friends: friends)
   end
 
   def create(conn, %{"friend" => friend_params}) do
     with {:ok, %Friend{} = friend} <- Friends.create_friend(friend_params) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/friends/#{friend}")
-      |> render(:show, friend: friend)
+      |> put_resp_header("location", ~p"/api/friends/#{friend.id}")
+      |> render("friend.json", friend: friend)
     end
   end
 
   def show(conn, %{"id" => id}) do
     friend = Friends.get_friend!(id)
-    render(conn, :show, friend: friend)
+    render(conn, "friend.json", friend: friend)
   end
 
   def update(conn, %{"id" => id, "friend" => friend_params}) do
     friend = Friends.get_friend!(id)
 
-    with {:ok, %Friend{} = friend} <- Friends.update_friend(friend, friend_params) do
-      render(conn, :show, friend: friend)
+    with {:ok, %Friend{} = updated_friend} <- Friends.update_friend(friend, friend_params) do
+      render(conn, "friend.json", friend: updated_friend)
     end
   end
 
@@ -46,33 +46,43 @@ defmodule ApiWeb.FriendController do
   # ---------------------------------------------- #
 
   def send_request(conn, %{"friend_id" => friend_id}) do
-    case Friends.send_friend_request(conn.assigns.current_user.id, friend_id) do
-      {:ok, _friend_request} -> json(conn, %{message: "Friend request sent"})
-      {:error, error} -> json(conn, %{error: error}, 400)
+    user_id = conn.assigns.current_user.id
+
+    with {:ok, %Friend{} = friend_request} <- Friends.send_friend_request(user_id, friend_id) do
+      render(conn, "friend.json", friend: friend_request)
+    else
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 
   def accept_request(conn, %{"friend_id" => friend_id}) do
-    case Friends.accept_friend_request(conn.assigns.current_user.id, friend_id) do
-      {:ok, _friendship} -> json(conn, %{message: "Friend request accepted"})
-      {:error, error} -> json(conn, %{error: error}, 400)
+    user_id = conn.assigns.current_user.id
+
+    with {:ok, %Friend{} = friendship} <- Friends.accept_friend_request(user_id, friend_id) do
+      render(conn, "friend.json", friend: friendship)
+    else
+      {:error, reason} -> {:error, reason}
     end
   end
 
   def decline_request(conn, %{"friend_id" => friend_id}) do
-    case Friends.decline_friend_request(conn.assigns.current_user.id, friend_id) do
-      {:ok, _} -> json(conn, %{message: "Friend request declined"})
-      {:error, error} -> json(conn, %{error: error}, 400)
+    user_id = conn.assigns.current_user.id
+
+    with {:ok, _} <- Friends.decline_friend_request(user_id, friend_id) do
+      send_resp(conn, :no_content, "")
+    else
+      {:error, reason} -> {:error, reason}
     end
   end
 
   def list_friends(conn, _params) do
     friends = Friends.get_friends(conn.assigns.current_user.id)
-    json(conn, friends)
+    render(conn, "friend.json", friends: friends)
   end
 
   def list_pending_requests(conn, _params) do
     requests = Friends.get_pending_friend_requests(conn.assigns.current_user.id)
-    json(conn, requests)
+    render(conn, "friend_requests.json", requests: requests)
   end
 end
--- api/lib/api_web/controllers/friend_json.ex
+++ api/lib/api_web/controllers/friend_json.ex
@@ -12,12 +12,23 @@ defmodule ApiWeb.FriendJSON do
   Renders a single friend.
   """
   def show(%{friend: friend}) do
-    %{data: data(friend)}
+    %{friend: data(friend)}
+  end
+
+  @doc """
+  Renders a list of pending friend requests.
+  """
+  def friend_requests(%{requests: requests}) do
+    %{friend_requests: for(request <- requests, do: data(request))}
   end
 
   defp data(%Friend{} = friend) do
     %{
-      id: friend.id
+      id: friend.id,
+      user_id: friend.user_id,
+      friend_id: friend.friend_id,
+      status: friend.status,
+      friends_since: friend.inserted_at
     }
   end
 end
