214ac10c9f9fc79379b9ff76dd76a5f9c3396c35
Aaliyah Harper <aeverly14@pm.me>
2025-03-16 23:16:48 -0400

    remove interval from supervision tree

--- backend/lib/api/application.ex
+++ backend/lib/api/application.ex
@@ -23,7 +23,7 @@ defmodule Api.Application do
       # {Api.Worker, arg},
       # Start to serve requests, typically the last entry
       ApiWeb.Endpoint,
-      {Guardian.DB.Sweeper, [interval: 60_000]}
+      {Guardian.DB.Sweeper, []}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
