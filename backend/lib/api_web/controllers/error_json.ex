2e4d6f2174755a948c37b1f9feda75457f19a974
Aaliyah Harper <aeverly14@pm.me>
2025-03-18 14:29:45 -0400

    fix errors in prod

--- backend/lib/api_web/controllers/error_json.ex
+++ backend/lib/api_web/controllers/error_json.ex
@@ -43,10 +43,4 @@ defmodule ApiWeb.ErrorJSON do
       end)
     end)
   end
-
-  defp translate_error({msg, opts}) do
-    Enum.reduce(opts, msg, fn {key, value}, acc ->
-      String.replace(acc, "%{#{key}}", to_string(value))
-    end)
-  end
 end
