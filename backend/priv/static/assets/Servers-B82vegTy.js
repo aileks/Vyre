6cb0923138ce5d4730e08f99a2aed0f9e43dfe64
Aaliyah Harper <aeverly14@pm.me>
2025-03-18 20:07:45 -0400

    rebuild

--- backend/priv/static/assets/Servers-B82vegTy.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:backend/priv/static/assets/Servers-nev5NvDB.js
-import{t}from"./index-B_UNcvHN.js";var e=t('<div><h1 class="mt-12 ml-12 text-2xl font-bold">Hello from Servers');function l(){return e()}export{l as default};
-========
-import{t}from"./index-DhWBrHsg.js";var e=t('<div><h1 class="mt-12 ml-12 text-2xl font-bold">Hello from Servers');function l(){return e()}export{l as default};
->>>>>>>> servers:backend/priv/static/assets/Servers-B82vegTy.js
---
+++ backend/priv/static/assets/Servers-BAdD4TG-.js
@@ -0,0 +1,1 @@
+import{t}from"./index-C4eSQ4c0.js";var e=t('<div><h1 class="mt-12 ml-12 text-2xl font-bold">Hello from Servers');function l(){return e()}export{l as default};
